ad5ae5f309b87de82d7ff3ccb90aebf50a9c0e19
Thor Pedersen <technostamper@hotmail.com>
2016-02-02 11:16:43 +0100

    New push

--- Dynamic buttons test 2/MainWindow.xaml.cs
+++ Dynamic buttons test 2/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -29,6 +30,7 @@ namespace Dynamic_buttons_test_2
             
             InitializeComponent();
             LoadXamlMethod();
+            
         }
 
         ///
@@ -87,6 +89,33 @@ namespace Dynamic_buttons_test_2
             }
         }
 
+        private void LoadClickEvents()
+        {
+            //BtnOs.Click += BtnOs_Click;
+            //BtnComputerName.AddHandler(Button.ClickEvent, new RoutedEventHandler(BtnComputerName_Click));
+            //BtnVersions.AddHandler(Button.ClickEvent, new RoutedEventHandler(BtnVersions_Click));
+            //BtnSave.AddHandler(Button.ClickEvent, new RoutedEventHandler(BtnSaveXaml_Click));
+
+            // Button button  = (Button)this.TryFindResource("BtnWindows99");
+
+            var ButtonnOs = TryFindResource("BtnWindows7") as Button;
+            if (ButtonnOs == null) { throw new Exception("Button not found"); }
+            ButtonnOs.Click += BtnOs_Click;
+
+            //Button buttonnOs = LayoutRoot.FindName("BtnOs") as Button;
+            //MessageBox.Show(buttonnOs.Name);
+            //if (buttonnOs == null) { throw new Exception("Button not found"); }
+            //buttonnOs.AddHandler(ButtonBase.ClickEvent, new RoutedEventHandler(BtnOs_Click));
+
+            //BtnWindows7.AddHandler(Button.ClickEvent, new RoutedEventHandler(btnOs3_click));
+            //BtnWindows8.AddHandler(Button.ClickEvent, new RoutedEventHandler(btnOs2_click));
+            //BtnWindows10.AddHandler(Button.ClickEvent, new RoutedEventHandler(btnOs1_click));
+
+            //BtnVersion1.AddHandler(Button.ClickEvent, new RoutedEventHandler(Version1_click));
+            //BtnVersion2.AddHandler(Button.ClickEvent, new RoutedEventHandler(Version2_click));
+            //BtnVersion3.AddHandler(Button.ClickEvent, new RoutedEventHandler(Version3_click));
+
+        }
         private void stackVisibility(Grid name)
         {
             name.Visibility = name.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
@@ -103,12 +132,15 @@ namespace Dynamic_buttons_test_2
         {
             stackVisibility(VersionStackPanel);
         }
-
         private void BackgroundChange(Button name)
         {
             name.Background = name.Background == Brushes.Gray ? Brushes.Gainsboro : Brushes.Gray;
         }
 
+        private void ListofClicks()
+        {
+            
+        }
         private void Version1_click(object sender, RoutedEventArgs e)
         {
             BackgroundChange(BtnVersion1);           
@@ -121,22 +153,18 @@ namespace Dynamic_buttons_test_2
         {
             BackgroundChange(BtnVersion3);
         }
-
         private void btnOs1_click(object sender, RoutedEventArgs e)
         {
             BackgroundChange(BtnWindows10);
         }
-
         private void btnOs2_click(object sender, RoutedEventArgs e)
         {
             BackgroundChange(BtnWindows8);
         }
-
         private void btnOs3_click(object sender, RoutedEventArgs e)
         {
             BackgroundChange(BtnWindows7);
         }
-
         private void BtnSaveXaml_Click(object sender, RoutedEventArgs e)
         {
             try
@@ -157,6 +185,11 @@ namespace Dynamic_buttons_test_2
             {
                 MessageBox.Show("Xaml saved.");
             }
+        }
+
+        private void Load_events_click(object sender, RoutedEventArgs e)
+        {
+            LoadClickEvents();
         }
     }
 }
